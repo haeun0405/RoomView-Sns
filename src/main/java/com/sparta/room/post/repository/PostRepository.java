8dceaa7133baa38f74f13f9cfcabba57e36b5123
pie0902 <zhfoqkq@gmail.com>
2024-02-23 14:12:52 +0900

    :tada: init

--- src/main/java/com/sparta/room/post/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.room.post.repository;
-
-import com.sparta.room.post.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post,Long> {
-
-}
--- src/main/java/com/sparta/room/user/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.room.user.repository;
-
-import com.sparta.room.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}
