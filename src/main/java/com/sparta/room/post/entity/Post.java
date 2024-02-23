ce61a2731c49c11bf40aa010d4e09413c7433bbe
pie0902 <zhfoqkq@gmail.com>
2024-02-23 14:08:39 +0900

    :art:init

---
+++ src/main/java/com/sparta/room/post/entity/Post.java
@@ -0,0 +1,22 @@
+package com.sparta.room.post.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name="posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String content;
+}
